ff54b876538b91291a41800212006a196a97eb50
tiagobarros01 <tbarros1337@hotmail.com>
2021-06-21 21:51:24 -0300

    chore: Wrappers the App with ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 
 import Home from './pages/Home/Index';
+import DefaultTheme from './styles/themes/DefaultTheme';
 
-const App = (): JSX.Element => <Home />;
+const App = (): JSX.Element => (
+  <ThemeProvider theme={DefaultTheme}>
+    <Home />
+  </ThemeProvider>
+);
 
 export default App;
