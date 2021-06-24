9c76e3bc40fe30970994b659b0879f7dbe07980f
tiagobarros01 <tbarros1337@hotmail.com>
2021-06-24 01:03:03 -0300

    feat: Adds AdminRoompage to routes

--- src/routes/index.routes.tsx
+++ src/routes/index.routes.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import AdminRoom from '../pages/AdminRoom';
 import Home from '../pages/Home/Index';
 import NewRoom from '../pages/NewRoom';
 import Room from '../pages/Room';
@@ -12,6 +13,8 @@ export default function Routes(): JSX.Element {
         <Route path="/" component={Home} exact />
         <Route path="/rooms/new" component={NewRoom} />
         <Route path="/rooms/:id" component={Room} />
+
+        <Route path="/admin/rooms/:id" component={AdminRoom} />
       </Switch>
     </BrowserRouter>
   );
