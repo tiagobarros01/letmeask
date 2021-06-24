1fc8f717986d0e0e5ce208df69237db79c8c0d0f
tiagobarros01 <tbarros1337@hotmail.com>
2021-06-24 02:27:17 -0300

    feat: Creates a remove question feat

--- src/pages/AdminRoom/index.tsx
+++ src/pages/AdminRoom/index.tsx
@@ -1,12 +1,15 @@
+/* eslint-disable no-alert */
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
+import DeleteImg from '../../assets/images/delete.svg';
 import LogoImg from '../../assets/images/logo.svg';
 import { Button } from '../../components/Button';
 import { Question } from '../../components/Question';
 import { RoomCode } from '../../components/RoomCode';
 // import { useAuth } from '../../hooks/useAuth';
 import { useRoom } from '../../hooks/useRoom';
+import { database } from '../../services/firebase';
 // import { database } from '../../services/firebase';
 import {
   Wrapper,
@@ -26,6 +29,12 @@ export default function AdminRoom(): JSX.Element {
   const { id: roomId } = useParams<RoomParams>();
 
   const { questions, title } = useRoom(roomId);
+
+  async function handleDeleteQuestion(questionId: string) {
+    if (window.confirm('Are you sure you want to delete this question')) {
+      await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+    }
+  }
 
   return (
     <Wrapper>
@@ -59,7 +68,14 @@ export default function AdminRoom(): JSX.Element {
               content={question.content}
               author={question.author}
               key={question.id}
-            />
+            >
+              <button
+                type="button"
+                onClick={() => handleDeleteQuestion(question.id)}
+              >
+                <img src={DeleteImg} alt="Remove question" />
+              </button>
+            </Question>
           ))}
         </List>
       </Main>
